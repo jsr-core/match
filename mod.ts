150c51678e634b1da75fe251f4f62dde1688cbac
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-03-05 22:50:12 +0000

    Add jsdoc

--- mod.ts
+++ mod.ts
@@ -5,24 +5,41 @@ type Pred = (v: unknown) => boolean;
 type Is<T> = (v: unknown) => v is T;
 
 /**
- * Placeholder class that holds a name and a test function.
+ * RegularPlaceholder class that holds a name and a test function.
  * 
- * @template T - The type of the key, which extends Key.
- * @template V - The type of the test function, which extends Pred.
+ * @template T - Name of the placeholder.
+ * @template V - Type guard function.
  */
 export class RegularPlaceholder<T extends Key, V extends Pred = Is<unknown>> {
   name: T;
   test?: V;
+
+  /**
+   * Represents a class constructor.
+   * @param name - The name parameter.
+   * @param [test] - The test parameter (optional).
+   */
   constructor(name: T, test?: V) {
     this.name = name;
     this.test = test;
   }
 }
 
+/**
+ * Represents a template string placeholder with optional regular placeholders.
+ * @template T - Tuple of regular placeholders.
+ */
 export class TemplateStringPlaceholder<T extends RegularPlaceholder<Key>[]> {
   strings: TemplateStringsArray;
   placeholders: T;
   greedy: boolean;
+
+  /**
+   * Creates a new instance of TemplateStringPlaceholder.
+   * @param greedy - Indicates whether the matching should be greedy or not.
+   * @param strings - The template strings array.
+   * @param placeholders - The regular placeholders.
+   */
   constructor(greedy: boolean, strings: TemplateStringsArray, ...placeholders: T) {
     this.greedy = greedy;
     this.strings = strings;
@@ -30,15 +47,52 @@ export class TemplateStringPlaceholder<T extends RegularPlaceholder<Key>[]> {
   }
 }
 
+/**
+ * Creates a placeholder with the specified name and test function.
+ * @template T - The type of the placeholder name.
+ * @template V - The type of the test function.
+ * @param name - The name of the placeholder.
+ * @param test - The test function to be used for matching.
+ * @returns {RegularPlaceholder<T, V>} - The created placeholder.
+ */
 export function placeholder<T extends Key, V extends Pred = Is<unknown>>(name: T, test?: V): RegularPlaceholder<T, V>;
+
+/**
+ * Creates a placeholder template string with non-greedy matching.
+ *
+ * @template T - The type of the placeholders.
+ * @param strings - The template strings array.
+ * @param placeholders - The placeholders.
+ * @returns The template string placeholder.
+ */
 export function placeholder<T extends RegularPlaceholder<Key>[]>(strings: TemplateStringsArray, ...placeholders: T): TemplateStringPlaceholder<T>;
-export function placeholder<T extends Key, U extends RegularPlaceholder<Key>[], V extends Pred = Is<unknown>>(name: T, ...test: [U] | U): RegularPlaceholder<T, V> | TemplateStringPlaceholder<U> {
-  if (name instanceof Array) {
-    return new TemplateStringPlaceholder(false, name as any, ...test as any);
+
+/**
+ * The implementation of the placeholder function.
+ * @param nameOrStrings - The name of the placeholder or the template strings array.
+ * @param testOrPlaceholders - The test function or the placeholders.
+ * @returns The created placeholder.
+ * @template T - The type of the placeholder name.
+ * @template U - The type of the placeholders.
+ * @template V - The type of the test function.
+ */
+export function placeholder<T extends Key, U extends RegularPlaceholder<Key>[], V extends Pred = Is<unknown>>(nameOrStrings: T, ...testOrPlaceholders: [U] | U): RegularPlaceholder<T, V> | TemplateStringPlaceholder<U> {
+  if (nameOrStrings instanceof Array) {
+    return new TemplateStringPlaceholder(false, nameOrStrings as any, ...testOrPlaceholders as any);
   }
-  return new RegularPlaceholder(name, ...test as any);
+  return new RegularPlaceholder(nameOrStrings, ...testOrPlaceholders as any);
 }
-function greedy<T extends RegularPlaceholder<Key>[]>(strings: TemplateStringsArray, ...placeholders: T): TemplateStringPlaceholder<T> {
+
+/**
+ * Creates a placeholder template string with greedy matching.
+ * This function could be used as `placeholder.greedy`.
+ * 
+ * @template T - The type of the placeholders.
+ * @param strings - The template strings array.
+ * @param placeholders - The placeholders.
+ * @returns A new TemplateStringPlaceholder instance.
+ */
+export function greedy<T extends RegularPlaceholder<Key>[]>(strings: TemplateStringsArray, ...placeholders: T) {
   return new TemplateStringPlaceholder(true, strings, ...placeholders);
 }
 placeholder.greedy = greedy;
@@ -47,6 +101,8 @@ placeholder.greedy = greedy;
  * Result type is a recursive type that represents the result of `match` function.
  * This type is a record of keys and values that are matched.
  * All the keys are declared as placeholders in `P` and the values are the types of the matched values.
+ * 
+ * @template P - The pattern to match against.
  */
 export type Result<P> =
   P extends RegularPlaceholder<infer V, Is<infer U>> ? { [v in V]: U } :
@@ -68,7 +124,7 @@ type Loop<P, Acc extends object = {}> =
 export function match<T>(pattern: T, target: any): Result<T> | undefined {
   if (pattern instanceof RegularPlaceholder) {
     if (!pattern.test || pattern.test(target)) {
-      return { [pattern.name]: target } as any;
+      return { [pattern.name]: target } as Result<T>;
     }
     return undefined;
   }
@@ -77,43 +133,38 @@ export function match<T>(pattern: T, target: any): Result<T> | undefined {
       return undefined;
     }
     const sep = pattern.greedy ? '(.*)' : '(.*?)';
-    const re = `^${pattern.strings.map(s => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')).join(sep)}$`
-    const m = target.match(new RegExp(re));
-    if (!m) {
-      return undefined;
-    }
-    const result = {} as any;
-    for (let i = 0; i < pattern.placeholders.length; i++) {
+    const esc = (s: string) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const re = new RegExp(`^${pattern.strings.map(esc).join(sep)}$`);
+    const m = target.match(re);
+    let result: Result<T> | undefined;
+    for (let i = 0; m && i < pattern.placeholders.length; i++) {
       const subResult = match(pattern.placeholders[i], m[i+1]);
       if (subResult) {
-        Object.assign(result, subResult);
+        result = { ...(result ?? {} as Result<T>), ...subResult };
         continue;
       }
       return undefined;
     }
     return result;
   }
-  if (pattern instanceof Array) {
-    const result = {} as any;
-    if (!(target instanceof Array) ||
-        pattern.length > target.length) {
-      return undefined;
-    }
-    for (let i = 0; i < pattern.length; i++) {
+  if (Array.isArray(pattern)) {
+    let result: Result<T> | undefined;
+    const ok = Array.isArray(target) && pattern.length <= target.length;
+    for (let i = 0; ok && i < pattern.length; i++) {
       const element = pattern[i];
       const subResult = match(element, target[i]);
       if (subResult) {
-        Object.assign(result, subResult);
+        result = { ...(result ?? {} as Result<T>), ...subResult };
         continue;
       }
       return undefined;
     }
     return result;
   }
-  if (pattern instanceof Object && 
-      target instanceof Object && 
-      Object.getPrototypeOf(pattern) === Object.getPrototypeOf({})) {
-    const result = {} as any;
+  if (pattern instanceof Object &&
+      target instanceof Object &&
+      Object.getPrototypeOf(pattern) === Object.prototype) {
+    const result = {} as Result<T>;
     for (const [key, value] of Object.entries(pattern)) {
       const subResult = match(value, target[key])
       if (key in target && subResult) {
